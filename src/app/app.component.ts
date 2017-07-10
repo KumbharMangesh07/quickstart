077b1eed4c73e47a90bbd1fc2fb5f218fc83f28d
cuzok <cuzook@gmail.com>
2017-07-10 11:33:02 +0530

    added ADD and DELETE functionalit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,36 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'my-app',
-  template: `<h1>Hello {{name}}</h1>`,
+  templateUrl: './app.component.html',
 })
-export class AppComponent  { name = 'Angular'; }
+
+// class List{
+
+//   content : string;
+//   highlight : boolean
+// }
+export class AppComponent  {
+   
+
+   newTodo : string;
+   name :string ; 
+   lists : string[] ;
+
+   constructor(){
+     
+     this.name = "TO-DO App";
+     this.lists = [];
+     this.newTodo =""
+   }
+   addList(){
+
+    this.lists.push(this.newTodo);
+    this.newTodo = ""; 
+
+   }
+
+   deleteList(index : number){
+
+     this.lists.splice(index,1);
+   }
+}
